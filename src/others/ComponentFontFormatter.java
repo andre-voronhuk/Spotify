c71898b01f5f8bc2917f277ff76353cd847f10b5
Rene Henrique <58603535+renehw@users.noreply.github.com>
2022-05-10 22:25:36 -0300

    Adicionado funcionalidade de criação de playList

--- src/others/ComponentFontFormatter.java
+++ src/others/ComponentFontFormatter.java
@@ -7,44 +7,30 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextField;
 
-public class ComponentFontFormatter{   
+public class ComponentFontFormatter {
 
     List<Component> componentes;
 
     public ComponentFontFormatter(Container c) {
         componentes = getAllComponents(c);
     }
-    
+
     public void format(Color primaryColor, ComponentesSwing cs) {
-//        if (cs == ComponentesSwing.JLABEL) {
-//            for (Component comp : componentes) {
-//                if (comp instanceof JLabel) {
-//                    JLabel j = (JLabel) comp;
-//                    j.setForeground(primaryColor);
-//                }
-//            }
-//        } else if (cs == ComponentesSwing.JTEXTFIELD) {
-//            for (Component comp : componentes) {
-//                if (comp instanceof JTextField) {
-//                    JTextField j = (JTextField) comp;
-//                    j.setBackground(primaryColor);
-//                }
-//            }
-//        }
 
         switch (cs) {
             case JLABEL:
-                for(Component comp : componentes) {
-                    if(comp instanceof JLabel) {
+                for (Component comp : componentes) {
+                    if (comp instanceof JLabel) {
                         JLabel j = (JLabel) comp;
                         j.setForeground(primaryColor);
                     }
                 }
-                
+
                 break;
-                
+
             case JTEXTFIELD:
                 for (Component comp : componentes) {
                     if (comp instanceof JTextField) {
@@ -53,7 +39,7 @@ public class ComponentFontFormatter{
                     }
                 }
                 break;
-                
+
             case JBUTTON:
                 for (Component comp : componentes) {
                     if (comp instanceof JButton) {
@@ -61,13 +47,20 @@ public class ComponentFontFormatter{
                         j.setBackground(primaryColor);
                     }
                 }
-                
+
+            case JLIST:
+                for (Component comp : componentes) {
+                    if (comp instanceof JList) {
+                        JList j = (JList) comp;
+                        j.setForeground(primaryColor);
+                    }
+                }
+
             default:
-              //  throw new AssertionError();
+            //  throw new AssertionError();
         }
     }
-    
-    
+
     private List<Component> getAllComponents(final Container c) {
         Component[] comps = c.getComponents();
         List<Component> compList = new ArrayList<Component>();
