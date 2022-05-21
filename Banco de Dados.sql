9ac4a1007b2edf36442150951d2fe2bf81520c25
André Voronhuk <andrevoronhuk@gmail.com>
2022-05-21 18:36:29 -0300

    Update Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -30,7 +30,7 @@ CREATE TABLE `albun` (
   `nome` varchar(255) NOT NULL,
   `artista` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `albun` (
 
 LOCK TABLES `albun` WRITE;
 /*!40000 ALTER TABLE `albun` DISABLE KEYS */;
-INSERT INTO `albun` VALUES (6,'The Girl','Flux Zone'),(7,'Black Sabbath 2009 Remaster','Black Sabbath');
+INSERT INTO `albun` VALUES (1,'Single','Single'),(6,'The Girl','Flux Zone'),(7,'Black Sabbath 2009 Remaster','Black Sabbath'),(8,'Underground','Flux Zone'),(9,'Black Sabbath','Black Sabbath'),(11,'Currents','Tame Impala'),(12,'Absolute Greatest','Queen'),(13,'Piece of Mind','Iron Maiden');
 /*!40000 ALTER TABLE `albun` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ CREATE TABLE `musica` (
   `estilo` varchar(100) NOT NULL,
   `albun_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `musica` (
 
 LOCK TABLES `musica` WRITE;
 /*!40000 ALTER TABLE `musica` DISABLE KEYS */;
-INSERT INTO `musica` VALUES (1,'N.I.B','Black Sabbath','musica/8.mp3','Rock',7),(2,'The Underground','Flux Zone','musica/1.mp3','house',0);
+INSERT INTO `musica` VALUES (1,'N.I.B','Black Sabbath','musica/8.mp3','Rock',7),(2,'The Underground','Flux Zone','musica/1.mp3','house',1),(4,'The Less I Know The Better','Tame Impala','musica/2.mp3','Alternative',11),(6,'Under Pressure ','Queen','musica/4.mp3','Rock',12),(7,'The Trooper','Iron Maiden','musica/3.mp3','Rock',13);
 /*!40000 ALTER TABLE `musica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +83,7 @@ CREATE TABLE `playlist` (
   `nome` varchar(200) NOT NULL,
   `dono_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +92,7 @@ CREATE TABLE `playlist` (
 
 LOCK TABLES `playlist` WRITE;
 /*!40000 ALTER TABLE `playlist` DISABLE KEYS */;
-INSERT INTO `playlist` VALUES (1,'teste',2),(2,'rock',2),(3,'Musicas Boas',1),(4,'House',1),(5,'pera ai',1),(6,'lo-fi',8),(7,'',8),(8,'',8),(9,'a',8),(10,'boa',1),(11,'nova playlist',2);
+INSERT INTO `playlist` VALUES (1,'teste',2),(2,'rock',2),(4,'House',1),(5,'pera ai',1),(6,'lo-fi',8),(7,'',8),(8,'',8),(9,'a',8),(10,'boa',1),(11,'nova playlist',2),(13,'Primeira playlist',9),(16,'testando',10);
 /*!40000 ALTER TABLE `playlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,7 +108,7 @@ CREATE TABLE `playlist_musica` (
   `musica_id` int(11) NOT NULL,
   `playlist_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +117,7 @@ CREATE TABLE `playlist_musica` (
 
 LOCK TABLES `playlist_musica` WRITE;
 /*!40000 ALTER TABLE `playlist_musica` DISABLE KEYS */;
-INSERT INTO `playlist_musica` VALUES (1,1,1),(2,1,3),(3,1,3),(4,1,3),(5,1,3);
+INSERT INTO `playlist_musica` VALUES (1,1,1),(2,1,3),(3,1,3),(4,1,3),(5,1,3),(6,2,4),(7,2,13),(8,2,14),(9,2,15),(10,2,16),(11,2,4),(12,2,4),(13,4,10);
 /*!40000 ALTER TABLE `playlist_musica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -135,7 +135,7 @@ CREATE TABLE `usuario` (
   `login` varchar(100) NOT NULL,
   `senha` varchar(70) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,7 +144,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Conta Admin','1','root','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(2,'Conta Usuario','0','user','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(8,'André Voronhuk','0','voronhuk','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB');
+INSERT INTO `usuario` VALUES (1,'Conta Admin','1','root','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(2,'Conta Usuario','0','user','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(8,'André Voronhuk','0','voronhuk','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(9,'Andre Voronhuk','1','voris','5970F1FE287D819DC05B370709F6206E88C0642091773A68BA8FDE60DE2686EB'),(10,'teste','0','teste','0351DC89488A475577B5C48756FE5D3CBC8551857EC196AE60168C07742A103C');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -157,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-18 16:57:23
+-- Dump completed on 2022-05-21 18:18:33
